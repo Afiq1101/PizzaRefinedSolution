f101cea516b5c672142fb7a1810d799f1cd673ee
Afiq1101 <59306394+Afiq1101@users.noreply.github.com>
2022-12-16 02:51:10 +0800

    Update Topping.java

--- src/toppings/Topping.java
+++ src/toppings/Topping.java
@@ -2,6 +2,8 @@ package toppings;
 
 import java.util.ArrayList;
 
+import pizza.Pizza;
+
 public abstract class  Topping extends Pizza {
 
 	public abstract ArrayList<String> addTops(ArrayList<String> currentToppings);
